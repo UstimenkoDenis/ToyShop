0e33ff44a168bfc135e3ab5f1b9948d16230c40f
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-09-26 23:04:16 +0300

    feat: add output to page

--- ToyShop/Startup.cs
+++ ToyShop/Startup.cs
@@ -19,7 +19,8 @@ namespace ToyShop
         public void ConfigureServices(IServiceCollection services) {
             services.AddTransient<IAllToys, MockToys>();
             services.AddTransient<IToysCategory, MockCategory>();
-            services.AddMvc();
+           // services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
