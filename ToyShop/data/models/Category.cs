d30cfa58299698a5e130e6dec2c8cfb8b57e8bf6
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-09-26 15:09:10 +0300

    feat: Category model complete

--- ToyShop/data/models/Category.cs
+++ ToyShop/data/models/Category.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ToyShop.data.models
-{
-    public class Category
-    {
+namespace ToyShop.data.models {
+    public class Category {
+        public int id { set; get; }
+        public string categoryName  { set; get; }
+        public string desc { set; get; }
+
+        public List<Toy> toys { set; get; }
+
     }
 }
