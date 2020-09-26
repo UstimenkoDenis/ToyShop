967ebadc6c828a6bdedcb67dc5e12aab3202cd30
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-09-26 16:28:31 +0300

    feat: add mock Category

---
+++ ToyShop/data/mocks/MockCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToyShop.data.interfaces;
+using ToyShop.data.models;
+
+namespace ToyShop.data.mocks {
+    public class MockCategory : IToysCategory {
+        public IEnumerable<Category> allCategories {
+            get {
+                return new List<Category> {
+                    new Category { categoryName = "Младенцы", desc = "Дети до одного года"},
+                    new Category { categoryName = "Активная игра", desc = "Игрушки для детей от года до трёх"}
+                }
+            }
+        }
+    }
+}
