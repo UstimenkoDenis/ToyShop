ac07a387d7e5cdf50d7acacce1bd85f59392a8ca
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-09-26 15:51:28 +0300

    feat: add interface IAllToys

---
+++ ToyShop/data/interfaces/IAllToys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToyShop.data.models;
+
+namespace ToyShop.data.interfaces {
+    interface IAllToys {
+
+        IEnumerable<Toy> Toys { get; set }
+        IEnumerable<Toy> getFavToy { get; set }
+        Toy getObjectToy(int toyId);
+
+    }
+}
