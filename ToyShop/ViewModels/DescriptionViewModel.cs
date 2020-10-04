bd079c95c08aec132daca56e291fcd4996a591f8
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-10-04 11:49:27 +0300

    feat: add description for toys

---
+++ ToyShop/ViewModels/DescriptionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToyShop.data.models;
+
+namespace ToyShop.ViewModels {
+    public class DescriptionViewModel {
+       public Toy getObjectToy { get; set; }
+       public string available { get; set; }
+    }
+}
---
+++ ToyShop/controllers/DescriptionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToyShop.data.interfaces;
+using ToyShop.ViewModels;
+
+namespace ToyShop.controllers
+{
+    public class DescriptionController : Controller {
+
+        private IAllToys _toyRep;
+        public DescriptionController(IAllToys toyRep)
+        {
+            _toyRep = toyRep;
+            
+        }
+        
+        public ViewResult Index(int id) {
+            string theToyAvail;
+
+            if (_toyRep.getObjectToy(id).available) {
+                theToyAvail = "Есть в наличии";
+            } else {
+                theToyAvail = "Нет в наличии";
+            }
+
+                var theToy = new DescriptionViewModel { 
+                getObjectToy = _toyRep.getObjectToy(id),
+                available = theToyAvail
+                };
+            return View(theToy);
+        }
+    }
+}
